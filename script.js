34c85dfc818a72ecdebfd2e208cc373192b85fc9
safdar imam <safdarimam486@gmail.com>
2024-05-23 11:03:44 +0530

    commit

---
+++ script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 60){
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+})
+
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+// const locoScroll = new LocomotiveScroll({
+//   el: document.querySelector(".smooth-scroll"),
+//   smooth: true
+// });
+// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+// locoScroll.on("scroll", ScrollTrigger.update);
+
+// // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+// ScrollTrigger.scrollerProxy(".smooth-scroll", {
+//   scrollTop(value) {
+//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//   getBoundingClientRect() {
+//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//   },
+//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//   pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+// });
+
+
+
+
+// tl.from(".purple p", {scale: 0.3, rotation:45, autoAlpha: 0, ease: "power2"})
+//   .from(".line-3", {scaleX: 0, transformOrigin: "left center", ease: "none"}, 0)
+//   .to(".purple", {backgroundColor: "#28a92b"}, 0);
+
+
+
+// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+// ScrollTrigger.refresh();
